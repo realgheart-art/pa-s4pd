23721f7c1498868a66efa54d6c3a7f6b37ed0ea5
realgheart-art <realgheart@gmail.com>
2026-06-11 12:59:06 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+/*  PA S4PD — Tetapan
+ *  Selepas deploy Google Apps Script sebagai Web App,
+ *  tampal URL Web App tersebut di bawah (ganti teks dalam tanda petik).
+ */
+window.PA_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbwW73Tmp2di6I-feqc4MWFtPYsrlLpk9wReZZj5ODpN-Z71_H4SDpYyPE1a5G7Me5p6/exec'
+};
